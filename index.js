28994917ae8fbf71d4e3e9b00f31bee2131237ac
devtototh <devtototh@gmail.com>
2024-02-27 01:41:51 +0700

    Add example game

---
+++ index.js
@@ -0,0 +1,425 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+(lib.Dead1 = function() {
+	this.initialize(img.Dead1);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead10 = function() {
+	this.initialize(img.Dead10);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead11 = function() {
+	this.initialize(img.Dead11);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead12 = function() {
+	this.initialize(img.Dead12);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead13 = function() {
+	this.initialize(img.Dead13);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead14 = function() {
+	this.initialize(img.Dead14);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead15 = function() {
+	this.initialize(img.Dead15);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead16 = function() {
+	this.initialize(img.Dead16);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead17 = function() {
+	this.initialize(img.Dead17);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead2 = function() {
+	this.initialize(img.Dead2);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead3 = function() {
+	this.initialize(img.Dead3);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead4 = function() {
+	this.initialize(img.Dead4);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead5 = function() {
+	this.initialize(img.Dead5);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead6 = function() {
+	this.initialize(img.Dead6);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead7 = function() {
+	this.initialize(img.Dead7);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead8 = function() {
+	this.initialize(img.Dead8);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Dead9 = function() {
+	this.initialize(img.Dead9);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,934,641);
+
+
+(lib.Symbol1 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// timeline functions:
+	this.frame_16 = function() {
+		/* Stop at This Frame
+		The  timeline will stop/pause at the frame where you insert this code.
+		Can also be used to stop/pause the timeline of movieclips.
+		*/
+		
+		this.stop();
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).wait(16).call(this.frame_16).wait(1));
+
+	// s1
+	this.instance = new lib.Dead1();
+	this.instance.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_1 = new lib.Dead2();
+	this.instance_1.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_2 = new lib.Dead3();
+	this.instance_2.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_3 = new lib.Dead4();
+	this.instance_3.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_4 = new lib.Dead5();
+	this.instance_4.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_5 = new lib.Dead6();
+	this.instance_5.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_6 = new lib.Dead7();
+	this.instance_6.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_7 = new lib.Dead8();
+	this.instance_7.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_8 = new lib.Dead9();
+	this.instance_8.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_9 = new lib.Dead10();
+	this.instance_9.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_10 = new lib.Dead11();
+	this.instance_10.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_11 = new lib.Dead12();
+	this.instance_11.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_12 = new lib.Dead13();
+	this.instance_12.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_13 = new lib.Dead14();
+	this.instance_13.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_14 = new lib.Dead15();
+	this.instance_14.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_15 = new lib.Dead16();
+	this.instance_15.setTransform(0,0,0.5944,0.5944);
+
+	this.instance_16 = new lib.Dead17();
+	this.instance_16.setTransform(0,0,0.5944,0.5944);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_3}]},1).to({state:[{t:this.instance_4}]},1).to({state:[{t:this.instance_5}]},1).to({state:[{t:this.instance_6}]},1).to({state:[{t:this.instance_7}]},1).to({state:[{t:this.instance_8}]},1).to({state:[{t:this.instance_9}]},1).to({state:[{t:this.instance_10}]},1).to({state:[{t:this.instance_11}]},1).to({state:[{t:this.instance_12}]},1).to({state:[{t:this.instance_13}]},1).to({state:[{t:this.instance_14}]},1).to({state:[{t:this.instance_15}]},1).to({state:[{t:this.instance_16}]},1).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,555.2,381);
+
+
+// stage content:
+(lib.GameClient01 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	this.actionFrames = [0];
+	this.isSingleFrame = false;
+	// timeline functions:
+	this.frame_0 = function() {
+		if(this.isSingleFrame) {
+			return;
+		}
+		if(this.totalFrames == 1) {
+			this.isSingleFrame = true;
+		}
+		this.mv1.addEventListener("click", fl_MouseClickHandler.bind(this));
+		
+		function fl_MouseClickHandler()
+		{
+			// Start your custom code
+			// This example code displays the words "Mouse clicked" in the Output panel.
+			this.mv1.play();
+			
+			
+			fetch('http://localhost:8080/test/data', {
+				method: 'GET',
+				headers: {
+					'Accept': 'application/json',
+				}
+			})
+			.then(response => response.json())
+			.then(response => {
+				console.log(JSON.stringify(response));
+				alert(JSON.stringify(response));
+			});
+			
+				
+			
+			// End your custom code
+		}
+		
+		
+		//https://mocki.io/v1/5e6a6331-0a59-46e6-bbff-264498cb546e
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1));
+
+	// Layer_1
+	this.mv1 = new lib.Symbol1();
+	this.mv1.name = "mv1";
+	this.mv1.setTransform(158.45,205.55,1,1,0,0,0,277.6,190.5);
+
+	this.timeline.addTween(cjs.Tween.get(this.mv1).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(90.9,215.1,345.1,181.00000000000003);
+// library properties:
+lib.properties = {
+	id: '0D4FF400769972458B5F87B066FF4B87',
+	width: 420,
+	height: 400,
+	fps: 30,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/Dead1.png?1708972664313", id:"Dead1"},
+		{src:"images/Dead10.png?1708972664313", id:"Dead10"},
+		{src:"images/Dead11.png?1708972664313", id:"Dead11"},
+		{src:"images/Dead12.png?1708972664313", id:"Dead12"},
+		{src:"images/Dead13.png?1708972664313", id:"Dead13"},
+		{src:"images/Dead14.png?1708972664313", id:"Dead14"},
+		{src:"images/Dead15.png?1708972664313", id:"Dead15"},
+		{src:"images/Dead16.png?1708972664313", id:"Dead16"},
+		{src:"images/Dead17.png?1708972664313", id:"Dead17"},
+		{src:"images/Dead2.png?1708972664313", id:"Dead2"},
+		{src:"images/Dead3.png?1708972664313", id:"Dead3"},
+		{src:"images/Dead4.png?1708972664313", id:"Dead4"},
+		{src:"images/Dead5.png?1708972664313", id:"Dead5"},
+		{src:"images/Dead6.png?1708972664313", id:"Dead6"},
+		{src:"images/Dead7.png?1708972664313", id:"Dead7"},
+		{src:"images/Dead8.png?1708972664313", id:"Dead8"},
+		{src:"images/Dead9.png?1708972664313", id:"Dead9"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['0D4FF400769972458B5F87B066FF4B87'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
